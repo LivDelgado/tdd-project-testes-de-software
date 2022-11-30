4d18bada2a5387ee73b2b46b2989af1e43058e76
Lívia Delgado <23265664+LivDelgado@users.noreply.github.com>
2022-11-30 00:20:18 +0000

    Refatorando o Teste de Unidade

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -1,8 +1,9 @@
+import time
+
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
-import time
 
 MAX_WAIT = 5
 
@@ -16,13 +17,13 @@ class FunctionalTest(StaticLiveServerTestCase):
 
     def wait_for_row_in_list_table(self, row_text):
         start_time = time.time()
-        while True:  
+        while True:
             try:
-                table = self.browser.find_element_by_id('id_list_table')  
-                rows = table.find_elements_by_tag_name('tr')
+                table = self.browser.find_element_by_id("id_list_table")
+                rows = table.find_elements_by_tag_name("tr")
                 self.assertIn(row_text, [row.text for row in rows])
-                return  
-            except (AssertionError, WebDriverException) as e:  
-                if time.time() - start_time > MAX_WAIT:  
-                    raise e  
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
                 time.sleep(0.5)
--- functional_tests/test_layout_and_styling.py
+++ functional_tests/test_layout_and_styling.py
@@ -1,5 +1,7 @@
-from functional_tests.base import FunctionalTest
 from selenium.webdriver.common.keys import Keys
+
+from functional_tests.base import FunctionalTest
+
 
 class LayoutAndStylingTest(FunctionalTest):
     def test_layout_and_styling(self):
@@ -8,22 +10,17 @@ class LayoutAndStylingTest(FunctionalTest):
         self.browser.set_window_size(1024, 768)
 
         # Ela nota que o input box está centralizado
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.browser.find_element_by_id("id_new_item")
         self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width'] / 2,
-            512,
-            delta=10
+            inputbox.location["x"] + inputbox.size["width"] / 2, 512, delta=10
         )
 
         # Ela inicia uma nova lista e nota que o input
         # também está centralizado
-        inputbox.send_keys('testing')
+        inputbox.send_keys("testing")
         inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: testing')
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.wait_for_row_in_list_table("1: testing")
+        inputbox = self.browser.find_element_by_id("id_new_item")
         self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width'] / 2,
-            512,
-            delta=10
+            inputbox.location["x"] + inputbox.size["width"] / 2, 512, delta=10
         )
-
